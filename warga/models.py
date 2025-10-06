c4b333b7dc3e9f439f7059708ac8be253236f58b
Zalfa <zalfaanandita@gmail.com>
2025-10-06 23:12:36 +0700

    Praktikum 2

--- warga/models.py
+++ warga/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db import models
 
 class Warga(models.Model):
     nik = models.CharField(max_length=16, unique=True, verbose_name="Nomor Induk Kependudukan")
@@ -10,4 +9,24 @@ class Warga(models.Model):
 
     def __str__(self):
         return self.nama_lengkap
+    
+
+class Pengaduan(models.Model):
+    STATUS_CHOICES = [
+        ('BARU', 'Baru'),
+        ('DIPROSES', 'Diproses'),
+        ('SELESAI', 'Selesai'),
+    ]
+    
+    judul = models.CharField(max_length=200)
+    deskripsi = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='BARU')
+    tanggal_lapor = models.DateTimeField(auto_now_add=True)
+    
+    # Kunci relasinya ada di sini!
+    pelapor = models.ForeignKey(Warga, on_delete=models.CASCADE, related_name='pengaduan')
+
+    def __str__(self):
+        return self.judul
+
 # Create your models here.
--- warga/views.py
+++ warga/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Warga
+from .models import Warga, Pengaduan
 
 class WargaListView(ListView):
     model = Warga
     # Django secara otomatis akan mencari template di:
     # <nama_app>/<nama_model>_list.html -> warga/warga_list.html
-    
+
 class WargaDetailView(DetailView):
     model = Warga
+
+class PengaduanListView(ListView):
+    model = Pengaduan
+    template_name = 'warga/pengaduan_list.html'
 # Create your views here.
